0714b3c88b334254279715a2b0111384af8ccfe0
Kyeongwoong Ra <45453964+kyeongwoong@users.noreply.github.com>
2019-09-02 16:07:03 +0900

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -5,7 +5,6 @@ Created on Wed Jul 17 12:57:07 2019
 @author: samsung
 """
 
-
 import numpy as np
 from scipy.stats import norm
 import matplotlib.pyplot as plt
@@ -13,19 +12,16 @@ import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots(1, 1)
 
-#mean, var, skew, kurt = norm.stats(moments='mvsk')
-#x = np.linspace(norm.ppf(0.01),
-#                norm.ppf(0.99), 100)
-#ax.plot(x, norm.cdf(x),
-#        'r-', lw=5, alpha=0.6, label='norm pdf')
-
+# A사 입찰가 평균과 표준편자 
 A_mean = 0.8
 A_std = 0.10 
-B_mean = 0.9
-B_std = 0.10
+# B사 입찰가 평균과 표준편자 
+B_mean = 0.85
+B_std = 0.05
 
+#입찰가 평균 및 표준편차
 estim = 0.75
-estd = 0.10
+estd = 0.05
 
 x = np.linspace(0.5,1.0,51)
 
@@ -35,11 +31,14 @@ rel = norm.cdf((x-estim)/estd)
 
 profit = pwin*norm.pdf((x-estim)/estd)
 
-print(x,profit)
+max_profit=np.max(profit)
+max_index = np.where(max_profit==profit)
+
+print(max_profit, x[max_index],pwin[max_index])
 
 ax.plot(x, pwin,
         'r-', lw=5, alpha=0.6, label='Winning Probabilty')
-ax.plot(x, rel,
-        'b-', label='Reliability')
+#ax.plot(x, rel,
+#        'b-', lw=5, alpha=0.6, label='Reliability')
 ax.plot(x, profit,
-        'g-', label='Profit')
+        'g-', lw=5, alpha=0.6, label='Profit')
